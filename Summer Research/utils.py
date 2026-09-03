869cccd74e4165e7e0556c9edbfe1b1975dae3f6
HartySimon <none@none>
2026-09-03 11:32:26 -0700

    stopper and stuff

--- Summer Research/utils.py
+++ Summer Research/utils.py
@@ -261,3 +261,24 @@ class RunLogger:
         # Save back to disk
         with open(self.filepath, "w") as f:
             json.dump(data, f, indent=4)
+
+
+class EarlyStopper:
+# Source - https://stackoverflow.com/a/73704579
+# Posted by isle_of_gods, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-09-03, License - CC BY-SA 4.0
+    def __init__(self, patience=1, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float('inf')
+
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
